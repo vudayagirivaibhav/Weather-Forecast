0b9e8f96dc898dc694dc45fee773b7b7d4d5037e
Vaibhav Vudaygiri <vaibhav.vudayagiri@gmail.com>
2021-01-26 05:55:45 -0800

    Add weather api an d forecast api to fetch details

--- src/components/WeatherPage.js
+++ src/components/WeatherPage.js
@@ -1,32 +1,63 @@
 import React, { useState, useEffect } from 'react';
-import {getWeatherForecastData} from '../api/weatherApi';
+import axios from 'axios';
+import {BASE_URL, API_KEY} from '../../config';
 
 const WeatherPage = () => {
     const [city, setCity] = useState("San jose");
     const [weatherData, setWeatherData] = useState(null);
+    const [forecastData, setForecastData] = useState(null);
     const [loading,setLoading] = useState(false);
+    const [error,setError] = useState(null);
 
     const getWeatherInfo = async () => {
-       try {
-           const data = await getWeatherForecastData(city);
-           console.log('data',data);
-       }catch(error){
-           console.log(error.message);
-       }
+        setLoading(true);
+       let weatherUrl =  await axios.get(`${BASE_URL}/weather?q=${city}&appid=${API_KEY}`);
+       let forecastUrl = await  axios.get( `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}`);
+       axios
+       .all([weatherUrl, forecastUrl])
+       .then(
+         axios.spread((...responses) => {
+           let weatherResponse =  responses[0].data;
+            let  forecastResponse = responses[1].data.list;
+           setWeatherData(weatherResponse);
+           setForecastData(forecastResponse);
+           setLoading(false);
+         })
+       )
+       .catch(errors => {
+        setLoading(false);
+         // react on errors.
+         console.error(errors);
+       });
     }
-      
+    
+    useEffect(()=> {
+        getWeatherInfo();
+    },[]);
+
     return (
         <div>
             <div className="weather-div">
                 <input type="text" placeholder="Enter City name(ex: Dallas,San jose)" onChange={(e) => setCity(e.target.value)} />
                 <button type="button"  onClick={() => getWeatherInfo()}> Forecast </button>
             </div>
-            <div className="info-container">
-                 <h4>Live Weather Info</h4>
-            </div>
-            <div className="forecast-container">
+         {
+             loading ? <h1>Loading data</h1>:
+            weatherData? <div className="result-container">
+                 <div className="info-container">
+                 <h3>Current Weather Info</h3>
+                 <h4 className="city-name">{weatherData.name}</h4>
+                 <p className="description">{weatherData.weather[0].description}</p>
+                 <div className="description">
+                     <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}/>
+                 </div>
+                 <div className="forecast-container">
                  <h4>Forecast chart</h4>
             </div>
+            </div>
+                </div>:null
+         }   
+           
         </div>
 
     )
